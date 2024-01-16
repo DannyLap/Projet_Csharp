66708c0b5978f7fd1b91e7babab4f3beb0841a08
White_ONyme <112949958+DannyLap@users.noreply.github.com>
2024-01-16 23:50:33 +0100

    update Source

--- Source/Inventory.API/ApiServer.cs
+++ Source/Inventory.API/ApiServer.cs
@@ -7,41 +7,58 @@ using HttpListener = System.Net.HttpListener;
 public class ApiServer
 {
     private readonly HttpListener _listener = new HttpListener();
-    private readonly Thread _listenerThread;
+    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+    private readonly ManualResetEvent _listenerStoppedEvent = new ManualResetEvent(false);
 
     public ApiServer()
     {
         _listener.Prefixes.Add("http://localhost:8080/"); // Mettez le préfixe que vous souhaitez utiliser
-        _listenerThread = new Thread(HandleRequests);
     }
 
     public void Start()
     {
         _listener.Start();
-        _listenerThread.Start();
+
+        // Lancer le traitement des requêtes dans un nouveau thread
+        ThreadPool.QueueUserWorkItem(HandleRequests);
     }
 
     public void Stop()
     {
-        _listenerThread.Abort();
+        // Demander l'annulation et attendre que le traitement des requêtes soit terminé
+        _cancellationTokenSource.Cancel();
+        _listenerStoppedEvent.WaitOne();
+
         _listener.Stop();
     }
 
-    private void HandleRequests()
+    private void HandleRequests(object state)
     {
-        while (true)
+        try
         {
-            var context = _listener.GetContext(); // Bloque jusqu'à ce qu'une requête soit reçue
-            var response = context.Response;
+            while (!_cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                var context = _listener.GetContext(); // Bloque jusqu'à ce qu'une requête soit reçue
+                var response = context.Response;
 
-            // Traitez la requête ici
-            var responseBody = "Hello, World!";
-            var buffer = System.Text.Encoding.UTF8.GetBytes(responseBody);
+                // Traitez la requête ici
+                var responseBody = "Hello, World!";
+                var buffer = System.Text.Encoding.UTF8.GetBytes(responseBody);
 
-            response.ContentLength64 = buffer.Length;
-            var output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            output.Close();
+                response.ContentLength64 = buffer.Length;
+                var output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+        catch (HttpListenerException)
+        {
+            // L'exception HttpListenerException sera levée lorsque le listener sera fermé, ce qui est attendu lors de l'arrêt du serveur.
+        }
+        finally
+        {
+            _listenerStoppedEvent.Set(); // Indiquer que le traitement des requêtes est terminé
         }
     }
 }
+
